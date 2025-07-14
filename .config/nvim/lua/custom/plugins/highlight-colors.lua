13994c86df57b93d7dc9e71983c6f6fe3c6d7acc
Holden Greene <holdengreene26@gmail.com>
2025-07-13 23:27:11 -0600

    Added highlight colors plugin

---
+++ .config/nvim/lua/custom/plugins/highlight-colors.lua
@@ -0,0 +1,9 @@
+return {
+  'brenoprata10/nvim-highlight-colors',
+  opts = {
+    render = 'virtual',
+    virtual_symbol = '●',
+    virtual_symbol_position = 'eol',
+    virtual_symbol_suffix = '',
+  },
+}
