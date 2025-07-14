ce4592985997b67186d878cbbe3d6396d6f6a6f2
Holden Greene <holdengreene26@gmail.com>
2025-07-13 23:53:54 -0600

    Fancy alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,5 +2,14 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+if type -q starship
 starship init fish | source
+end
+
+if type -q bat
 set -x PAGER bat
+end
+
+if type -q nvim
+alias v="nvim"
+end
