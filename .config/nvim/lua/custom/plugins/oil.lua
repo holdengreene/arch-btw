33821a09beb3a9723815249c10bc67f71fa3dfdf
Holden Greene <hgreene@cmnhospitals.org>
2025-07-18 12:53:36 -0600

    Trying out oil.nvim

---
+++ .config/nvim/lua/custom/plugins/oil.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = { default_file_explorer = true },
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    lazy = false,
+    keys = {
+      {
+        '<leader>n',
+        '<cmd>Oil<cr>',
+        desc = 'Open Oil',
+      },
+    },
+  },
+}
