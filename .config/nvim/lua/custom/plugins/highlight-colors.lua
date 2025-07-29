e57a36af2fe702009038d59ac1daf04a66f42c12
Holden Greene <hgreene@cmnhospitals.org>
2025-07-29 13:54:55 -0600

    Cleaned up highlights

--- .config/nvim/lua/custom/plugins/highlight-colors.lua
+++ .config/nvim/lua/custom/plugins/highlight-colors.lua
@@ -1,10 +1,4 @@
 return {
   'brenoprata10/nvim-highlight-colors',
-  opts = {
-    render = 'virtual',
-    virtual_symbol = '●',
-    virtual_symbol_position = 'eow',
-    virtual_symbol_prefix = ' ',
-    virtual_symbol_suffix = '',
-  },
+  opts = {},
 }
